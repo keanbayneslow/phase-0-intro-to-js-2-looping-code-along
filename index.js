5df8f90ba20b7c870b631e3425b75c09398b0375
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-07-16 12:41:03 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Sarah", "Kean", "Bet", "Don", "Auret", "Katie", "Lilli"];
+const eventName = "Birthday";
+
+function writeCards(names, eventName) {
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        messages.push(message);
+    }
+
+    return messages;
+}
+
+function countDown(number) {
+    for (let i = 10; i >= 0; i--) {
+        console.log(i);
+    }
+}
